990b5701522641777646c75acdf89bfac0691be0
jeff <jefflustica2@gmail.com>
2023-08-12 01:45:07 +0800

    navbar bg color changed to white

--- src/components/CryptoCard.jsx
+++ src/components/CryptoCard.jsx
@@ -5,20 +5,29 @@ const CryptoCard = ({ coin }) => {
   return (
     <Link
       to={`/crypto/${coin.uuid}`}
-      className="flex flex-col justify-center gap-4 p-4 bg-gray-100"
+      className="flex flex-col justify-center gap-4 p-4 border rounded-lg shadow-[5px_5px_10px_-6px_rgba(0,0,0,0.3)]"
     >
       <div className="flex justify-between items-center">
         <div className="font-bold">
           {coin.rank}. {coin.name}
         </div>
-        <div className="w-8 h-8">
+        <div className="w-12 h-12">
           <img src={coin.iconUrl} />
         </div>
       </div>
-      <div>
+      <div className="text-gray-500">
         <div>Price: {millify(coin.price)}</div>
         <div>Market Cap: {millify(coin.marketCap)}</div>
-        <div>Daily Change: {millify(coin.change)}</div>
+        <div>
+          Daily Change:{" "}
+          <span
+            className={`font-bold ${
+              millify(coin.change) > 0 ? "text-green-500" : "text-red-500"
+            }`}
+          >
+            {millify(coin.change)}
+          </span>
+        </div>
       </div>
     </Link>
   );
--- src/components/DarkModeSwitch.jsx
+++ src/components/DarkModeSwitch.jsx
@@ -5,17 +5,17 @@ const DarkModeSwitch = () => {
   const isHover = useSelector((state) => state.isHover.isHover);
 
   return (
-    <div className="flex items-center justify-center sm:mt-auto sm:mb-4 sm:w-full sm:px-4">
+    <div className="flex items-center sm:mt-auto sm:mb-4 sm:w-full sm:px-4 sm:hover:bg-gray-100 p-3 sm:ml-1 group cursor-pointer">
+      <div className="group-hover:bg-accent-500 bg-text-primary-dark-900 p-2 rounded-lg max-sm:mr-4">
+        <HiMoon className="text-white" />
+      </div>
       <span
-        className={`mr-4 hidden whitespace-nowrap ${
+        className={`ml-4 hidden whitespace-nowrap ${
           isHover ? "sm:flex" : "sm:hidden"
         }`}
       >
         Dark Mode
       </span>
-      <div className="group bg-text-primary-dark-900 hover:bg-accent-500 p-2 rounded-lg cursor-pointer max-sm:mr-4">
-        <HiMoon className="text-white" />
-      </div>
     </div>
   );
 };
